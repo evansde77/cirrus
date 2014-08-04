f69f7e896fc5fc1c9994c9b62257157b45832dfe
Jordan Counts <jcounts24@gmail.com>
2014-08-04 16:47:48 -0500

    (WIP) now using mock instead of mox...still having issues

--- tests/unit/cirrus/git_tools_t.py
+++ tests/unit/cirrus/git_tools_t.py
@@ -1,16 +1,9 @@
 '''
-Created on Aug 4, 2014
-
-@author: jordancounts
+tests for git_tools
 '''
-import git
-import mox
-import os
-import posixpath
-import tempfile
+import mock
 import unittest
 
-import cirrus
 from cirrus.git_tools import checkout_and_pull
 # from tests.test_harness import GitTestHarness
 
@@ -18,25 +11,23 @@ from cirrus.git_tools import checkout_and_pull
 class GitToolsTest(unittest.TestCase):
 
     def setUp(self):
-        self.tempdir = tempfile.mkdtemp()
-        self.mox = mox.Mox()
-        self.repo = self.mox.CreateMockAnything()
-        self.mox.StubOutWithMock(
-            cirrus.git_tools.git,
-            'Repo',
-            use_mock_anything=True)
-        cirrus.git_tools.git.Repo(self.tempdir).AndReturn(self.repo)
-        self.mox.StubOutWithMock(self.repo.heads, 'master')
-        self.repo.heads.master().AndReturn(None)
+        self.repo_url = 'test/repo'
+        self.patch_git = mock.patch('cirrus.git_tools.git')
+        self.patch_git.start()
+        self.mock_repo = mock.Mock()
+        self.mock_repo.git_dir = self.repo_url
+        self.mock_repo.heads.master = mock.Mock()
+
+        self.patch_git.Repo = mock.Mock()
+        self.patch_git.Repo.return_value = self.mock_repo
 
     def tearDown(self):
-        self.mox.UnsetStubs()
-        os.rmdir(self.tempdir)
+        self.patch_git.stop()
 
     def test_checkout_and_pull(self):
-        self.mox.ReplayAll()
-        checkout_and_pull(self.tempdir, 'master')
-        self.mox.VerifyAll()
+        with mock.patch('cirrus.git_tools.checkout_and_pull') as mock_pull:
+            checkout_and_pull(self.repo_url, 'master')
+            self.failUnless(mock_pull.called)
 
 
 if __name__ == "__main__":
