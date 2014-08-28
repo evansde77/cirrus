4735c5870ebf045e7def2b4b97ee88cae93deecd
Jordan Counts <jcounts24@gmail.com>
2014-08-28 13:01:37 -0500

    more tests

--- tests/unit/cirrus/github_tools_t.py
+++ tests/unit/cirrus/github_tools_t.py
@@ -5,6 +5,7 @@ import mock
 import unittest
 
 from cirrus.github_tools import build_release_notes
+from cirrus.github_tools import create_pull_request
 
 OWNER = 'testorg'
 REPO = 'testrepo'
@@ -12,20 +13,45 @@ RELEASE = '0.0.0'
 
 
 class GithubToolsTest(unittest.TestCase):
+    """
+    _GithubToolsTest_
+    """
+
+    def test_create_pull_request(self):
+        """
+        _test_create_pull_request_
+        """
+        resp_json = {'html_url': 'https://github.com/{0}/{1}/pull/1'.format(OWNER, REPO)}
+        with mock.patch(
+            'cirrus.github_tools.load_configuration') as mock_config_load:
+
+            mock_config_load.organisation_name.return_value = OWNER
+            mock_config_load.package_name.return_value = REPO
+            with mock.patch(
+                'cirrus.github_tools.get_active_branch') as mock_get_branch:
+
+                with mock.patch(
+                    'cirrus.github_tools.requests.post') as mock_post:
+
+                    with mock.patch(
+                        'cirrus.github_tools.json.dumps') as mock_dumps:
+
+                        mock_post.raise_for_status.return_value = False
+                        mock_post.resp.json.return_value = resp_json
+                        create_pull_request(
+                            REPO,
+                            {'title': 'Test', 'body': 'This is a test'},
+                            'token')
+                        self.failUnless(mock_config_load.called)
+                        self.failUnless(mock_get_branch.called)
+                        self.failUnless(mock_post.called)
+                        self.failUnless(mock_dumps.called)
 
     def test_build_release_notes(self):
         """
         _test_build_release_notes_
         """
-        tag = {
-            'name': RELEASE,
-            'zipball_url': "https://api.github.com/repos/{0}/{1}/zipball/{2}".format(OWNER, REPO, RELEASE),
-            'tarball_url': "https://api.github.com/repos/{0}/{1}/tarball/{2}".format(OWNER, REPO, RELEASE),
-            'commit': {
-                'sha': "123abc",
-                'url': "https://api.github.com/repos/{0}/{1}/commits/abc123".format(OWNER, REPO)
-                }
-            }
+        tag = {RELEASE: "123abc"}
 
         with mock.patch(
             'cirrus.github_tools.get_tags_with_sha') as mock_get_tags_sha:
