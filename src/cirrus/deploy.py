845ebfcbb7eeecd0f485683d0189078d208f9444
Dave Evans <evansde77@gmail.com>
2015-04-08 15:14:09 -0500

    initial framework for deploy command, plugin fwk and chef deployer

---
+++ src/cirrus/deploy.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+"""
+_deploy_
+
+cirrus deployment command.
+
+Essentially a thin wrapper for the deploy_plugins
+module and its constituent bits
+
+"""
+import sys
+
+from argparse import ArgumentParser
+from cirrus.deploy_plugins import bootstrap_parser, get_plugin
+from cirrus.logger import get_logger
+
+
+LOGGER = get_logger()
+
+
+def build_parser(argslist):
+    """
+    _build_parser_
+
+    Set up command line parser for the deploy command
+
+    """
+    parser = ArgumentParser(
+        description='git cirrus deploy command'
+    )
+    subparsers = parser.add_subparsers(dest='command')
+    bootstrap_parser(subparsers)
+    opts = parser.parse_args(argslist)
+    return opts
+
+
+def main():
+    opts = build_parser(sys.argv)
+    plugin = get_plugin(opts.command)
+    plugin.deploy(opts)
+
+
+if __name__ == '__main__':
+    main()
