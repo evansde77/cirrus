205ffc3dd17691d9af27dade001e2e8b5f672586
Sean Hudgston <shudgston@cloudant.com>
2015-10-20 15:26:46 -0400

    Remove old plugin code and use external lib

--- src/cirrus/deploy.py
+++ src/cirrus/deploy.py
@@ -8,17 +8,14 @@ Essentially a thin wrapper for the deploy_plugins
 module and its constituent bits
 
 """
-import sys
-
 from argparse import ArgumentParser
 from cirrus.deploy_plugins import bootstrap_parser, get_plugin
 from cirrus.logger import get_logger
 
-
 LOGGER = get_logger()
 
 
-def build_parser(argslist):
+def build_parser():
     """
     _build_parser_
 
@@ -30,12 +27,12 @@ def build_parser(argslist):
     )
     subparsers = parser.add_subparsers(dest='command')
     bootstrap_parser(subparsers)
-    opts = parser.parse_args(argslist)
+    opts = parser.parse_args()
     return opts
 
 
 def main():
-    opts = build_parser(sys.argv)
+    opts = build_parser()
     plugin = get_plugin(opts.command)
     plugin.deploy(opts)
 
