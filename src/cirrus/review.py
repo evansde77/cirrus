c56004e226a0771fcf522f11d25a392628c0dcad
Dave Evans <evansde77@gmail.com>
2015-11-20 11:47:35 -0600

    implement review command

--- src/cirrus/review.py
+++ src/cirrus/review.py
@@ -7,8 +7,12 @@ including ability to set a specific status context for a branch
 to allow for confirmation of reviews for code compliance etc
 
 """
+import os
+import sys
+import pprint
 
 from argparse import ArgumentParser
+from cirrus.github_tools import GitHubContext
 
 
 def build_parser(argslist):
@@ -51,6 +55,13 @@ def build_parser(argslist):
         help='ID of pull request to review'
     )
     review_command.add_argument(
+        '--comment', '-m',
+        type=str,
+        required=True,
+        dest='comment',
+        help='Review comment text'
+    )
+    review_command.add_argument(
         '--plus-one', '-p',
         type=bool,
         default=False,
@@ -58,27 +69,94 @@ def build_parser(argslist):
         help='Add a plus-one status'
     )
     review_command.add_argument(
+        '--plus-one-context',
+        default='+1',
+        dest='plus_one_context',
+        help='Github context string to use as +1 tag'
+    )
+    plusone_command = subparsers.add_parser('plusone')
+    plusone_command.add_argument(
+        '--id', '-i',
+        required=True,
+        type=int,
+        dest='id',
+        help='ID of pull request to approve/+1'
+    )
+    plusone_command.add_argument(
         '--plus-one-context', '-c',
         default='+1',
         dest='plus_one_context',
         help='Github context string to use as +1 tag'
     )
+    opts = parser.parse_args(argslist)
+    return opts
 
 
 def list_prs(opts):
-    pass
+    """
+    _list_prs_
+
+    Print a summary of all open PRs for this repo
+    optionally filtered by user
+
+    """
+    repo_dir = os.getcwd()
+    with GitHubContext(repo_dir) as ghc:
+        print "  ID,  User, Title"
+        for pr in ghc.pull_requests(user=opts.user):
+            print pr['number'], pr['user']['login'], pr['title']
 
 
 def get_pr(opts):
-    pass
+    """
+    _get_pr_
+
+    Print the details of the pr specified by the CLI opts
+
+    """
+    repo_dir = os.getcwd()
+    with GitHubContext(repo_dir) as ghc:
+        pr_data = ghc.pull_request_details(opts.id)
+        pprint.pprint(pr_data, indent=2)
 
 
 def review_pr(opts):
-    pass
+    """
+    _review_pr_
 
+    Add a review comment to a PR, and optionally set the
+    plus one context status
+    """
+    repo_dir = os.getcwd()
+    with GitHubContext(repo_dir) as ghc:
+        ghc.review_pull_request(
+            opts.id,
+            opts.comment,
+            opts.plus_one,
+            opts.plus_one_context
+        )
+
+
+def plusone_pr(opts):
+    """
+    _plusone_pr_
+
+    Set the +1 context status for the PR
+    """
+    repo_dir = os.getcwd()
+    with GitHubContext(repo_dir) as ghc:
+        ghc.plus_one_pull_request(
+            pr_id=opts.id,
+            context=opts.plus_one_context
+        )
 
 
 def main():
+    """
+    _main_
+
+    call appropriate function based on command line opts
+    """
     opts = build_parser(sys.argv)
     if opts.command == 'list':
         list_prs(opts)
@@ -86,9 +164,9 @@ def main():
         review_pr(opts)
     if opts.command == 'details':
         get_pr(opts)
-
+    if opts.command == 'plusone':
+        plusone_pr(opts)
 
 
 if __name__ == '__main__':
-
     main()
