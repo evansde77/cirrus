2bb1399ecb0f6f19e2bf2cc158621d72172195f9
Dave Evans <evansde77@gmail.com>
2015-11-30 15:30:06 -0600

    support setting status for a given branch in a repo

--- src/cirrus/plusone.py
+++ src/cirrus/plusone.py
@@ -5,6 +5,8 @@ _plusone_
 repo/cirrus independent util for flagging PRs in github as plusone'd
 
 """
+import os
+import git
 import sys
 import json
 import argparse
@@ -44,6 +46,46 @@ class GitHubHelper(object):
         resp.raise_for_status()
         data = resp.json()
         return data
+
+    def set_branch_state(self, org, repo, context, repo_dir=None, branch=None):
+        """
+        _current_branch_mark_status_
+
+        Mark the CI status of the current branch.
+
+        :param state: state of the last test run, such as "success" or "failure"
+        :param context: The GH context string to use for the state, eg
+           "continuous-integration/travis-ci"
+
+        :param branch: Optional branch name or sha to set state on,
+           defaults to current active branch
+
+        """
+        if repo_dir is None:
+            repo_dir = os.getcwd()
+        git_repo = git.Repo(repo_dir)
+        if branch is None:
+            branch = git_repo.active_branch.name
+
+        # pull branch_from from remote
+        ref = "refs/heads/{0}:refs/remotes/origin/{0}".format(branch)
+        git_repo.remotes.origin.pull(ref)
+
+        sha = git_repo.head.commit.hexsha
+        url = "https://api.github.com/repos/{org}/{repo}/statuses/{sha}".format(
+            org=org,
+            repo=repo,
+            sha=sha
+        )
+        data = json.dumps(
+            {
+                "state": 'success',
+                "description": "State after cirrus check.",
+                "context": context
+            }
+        )
+        resp = self.session.post(url, data=data)
+        resp.raise_for_status()
 
     def plus_one(self, org, repo, sha, context, issue_url):
         """
@@ -89,7 +131,7 @@ def build_parser():
 
     parser.add_argument(
         '--id', '-i',
-        required=True,
+        default=None,
         type=int,
         dest='id',
         help='ID of pull request to approve/+1'
@@ -112,6 +154,19 @@ def build_parser():
         dest='org',
         help='Github Organisation name'
     )
+    parser.add_argument(
+        '--branch', '-b',
+        default=None,
+        dest='branch',
+        help='Branch name if not using a PR ID'
+    )
+    parser.add_argument(
+        '--repo-dir', '-d',
+        default=None,
+        dest='repo_dir',
+        help='Local repo path if not using a PR ID'
+    )
+
     opts = parser.parse_args()
     return opts
 
@@ -125,15 +180,31 @@ def main():
     """
     opts = build_parser()
     gh = GitHubHelper()
-    pr = gh.get_pr(opts.org, opts.repo, int(opts.id))
-    sha = pr['head']['sha']
-    issue_url = pr['issue_url']
-    created_by = pr["user"]["login"]
-    if created_by == gh.user:
+
+    if not (opts.id or opts.branch):
+        print opts.id,  opts.branch
+        msg = "Must supply either pull request ID or branch name"
+        print msg
+        sys.exit(1)
+
+    if opts.branch is not None:
+        gh.set_branch_state(
+            opts.org,
+            opts.repo,
+            opts.plus_one_context,
+            opts.repo_dir,
+            branch=opts.branch
+        )
+    else:
+        pr = gh.get_pr(opts.org, opts.repo, int(opts.id))
+        sha = pr['head']['sha']
+        issue_url = pr['issue_url']
+        created_by = pr["user"]["login"]
+        if created_by == gh.user:
             msg = "Reviewing your own Pull Requests is not allowed"
             raise RuntimeError(msg)
 
-    gh.plus_one(opts.org, opts.repo, sha, opts.plus_one_context, issue_url)
+        gh.plus_one(opts.org, opts.repo, sha, opts.plus_one_context, issue_url)
 
 
 if __name__ == '__main__':
