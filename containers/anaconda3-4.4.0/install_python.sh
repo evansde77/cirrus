310861cf0d68ae2ddda2b0405d8af65aa459648f
Dave Evans <dave.evans@imc.com>
2017-06-26 12:53:23 -0500

    update test suites

--- containers/anaconda3-4.4.0/install_python.sh
+++ containers/anaconda3-4.4.0/install_python.sh
@@ -9,9 +9,14 @@ eval "$(pyenv init -)"
 
 pyenv install anaconda3-4.4.0
 pyenv global anaconda3-4.4.0
-curl -L https://bootstrap.pypa.io/get-pip.py | bash
-pip install --upgrade pip setuptools virtualenv
-conda create venv
+curl -O https://bootstrap.pypa.io/get-pip.py
+python get-pip.py
+pip install --upgrade pip 
+pip install setuptools virtualenv
+
+
+
+
 
 
 
---
+++ containers/run_tests.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export REPO_DIR=`git rev-parse --show-toplevel`
+export CONTAINERS_DIR=${REPO_DIR}/containers
+
+
+function run_test(){
+    echo "Running ${1}..."
+    cd ${1}
+    docker build -t cirrus-${1}:latest .
+    docker run -ti --entrypoint /opt/scratch_test.sh cirrus-${1}:latest
+    docker run -ti --entrypoint /opt/clone_test.sh cirrus-${1}:latest
+    cd $CONTAINERS_DIR
+}
+
+
+declare -a VERSIONS=("py2.7" "py3.5" "py3.6")
+
+for i in "${VERSIONS[@]}"
+do
+   run_test $i
+done
